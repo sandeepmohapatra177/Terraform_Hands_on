d82e92fcfe9fcce46539ad55069a807d5683420c
Sandeep Mohapatra <mohapatra.sandeep007@gmail.com>
2025-10-07 13:53:25 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "demo-resources"
-    storage_account_name = "tfstatestorage177"
-    container_name       = "tfstate-container"
+    storage_account_name = "terraform177s"
+    container_name       = "tf-state"
     key                  = "terraform.tfstate"
   }
 }
